26391ef2024ce8a74504f6f4398aad210b851e1b
yangzilong <yangzilong@hungrypandagroup.com>
2022-04-29 16:26:56 +0800

    feat: 增加intersectionByKey方法

--- lodash/intersection.js
+++ lodash/intersection.js
@@ -1,11 +1,18 @@
-function intersection() {
-    var arrays = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        arrays[_i] = arguments[_i];
-    }
-    var result = [];
-    if (arrays.length < 2)
-        return result;
+// function intersection(...arrays: Array<Array<number>>): Array<number> {
+//     const result: Array<number> = [];
+//     if (arrays.length < 2) return result;
+//     let minLengthArrIndex = getArrayMinLengthIndex(arrays);
+//     const { minLengthArray, othArrays} = splitArrayWithMinIndex(arrays, minLengthArrIndex);
+//     for (let i = 0; i < minLengthArray.length; i++) {
+//         const isEventHas = othArrays.every(array => array.includes(minLengthArray[i]));
+//         if(isEventHas) {
+//             result.push(minLengthArray[i]);
+//         }
+//     }
+//     return result;
+// }
+// console.log(intersection([5],[1,2,3,4,5],[3,5]))
+function getArrayMinLengthIndex(arrays) {
     var minLengthArrIndex = 0;
     var minLengthArr = arrays[minLengthArrIndex];
     for (var i = 1; i < arrays.length; i++) {
@@ -15,17 +22,38 @@ function intersection() {
         }
     }
     ;
-    arrays.splice(minLengthArrIndex, 1);
-    var othArray = arrays;
+    return minLengthArrIndex;
+}
+function splitArrayWithMinIndex(arrays, minIndex) {
+    return {
+        minLengthArray: arrays[minIndex],
+        othArrays: arrays.filter(function (_, index) { return index !== minIndex; })
+    };
+}
+function intersectionByKey() {
+    var arg = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        arg[_i] = arguments[_i];
+    }
+    var result = [];
+    var arrays = arg.slice(0, arg.length - 1);
+    if (arrays.length < 2)
+        return result;
+    var key = arg[arg.length - 1];
+    var minLengthArrIndex = getArrayMinLengthIndex(arrays);
+    var _a = splitArrayWithMinIndex(arrays, minLengthArrIndex), minLengthArray = _a.minLengthArray, othArrays = _a.othArrays;
     var _loop_1 = function (i) {
-        var isEventHas = othArray.every(function (array) { return array.includes(minLengthArr[i]); });
-        if (isEventHas) {
-            result.push(minLengthArr[i]);
+        var comparator = function (array) { return array.find(function (item) { return item[key] === minLengthArray[i][key]; }); };
+        console.log(minLengthArray[i][key]);
+        var isEqual = othArrays.every(comparator);
+        if (isEqual) {
+            result.push(minLengthArray[i]);
         }
     };
-    for (var i = 0; i < minLengthArr.length; i++) {
+    for (var i = 0; i < minLengthArray.length; i++) {
         _loop_1(i);
     }
     return result;
 }
-console.log(intersection([5], [1, 2, 3, 4, 5], [3, 5]));
+var result = intersectionByKey([{ x: 1, y: 3 }, { x: 5, y: 5 }], [{ x: 1, y: 5 }, { x: 3, y: 1 }, { x: 5, y: 1 }, { x: 3, y: 5 }], [{ x: 1, y: 7 }, { x: 3, y: 3 }], [{ x: 3, y: 5 }, { x: 5 }], 'x');
+console.log(result);
--- lodash/intersection.ts
+++ lodash/intersection.ts
@@ -1,6 +1,19 @@
-function intersection(...arrays: Array<Array<number>>): Array<number> {
-    const result: Array<number> = [];
-    if (arrays.length < 2) return result;
+// function intersection(...arrays: Array<Array<number>>): Array<number> {
+//     const result: Array<number> = [];
+//     if (arrays.length < 2) return result;
+//     let minLengthArrIndex = getArrayMinLengthIndex(arrays);
+//     const { minLengthArray, othArrays} = splitArrayWithMinIndex(arrays, minLengthArrIndex);
+//     for (let i = 0; i < minLengthArray.length; i++) {
+//         const isEventHas = othArrays.every(array => array.includes(minLengthArray[i]));
+//         if(isEventHas) {
+//             result.push(minLengthArray[i]);
+//         }
+//     }
+//     return result;
+// }
+// console.log(intersection([5],[1,2,3,4,5],[3,5]))
+
+function getArrayMinLengthIndex(arrays: Array<Array<any>>) {
     let minLengthArrIndex = 0;
     let minLengthArr = arrays[minLengthArrIndex];
     for (let i = 1; i < arrays.length; i ++) {
@@ -9,15 +22,38 @@ function intersection(...arrays: Array<Array<number>>): Array<number> {
             minLengthArr = arrays[i];
         }
     };
-    arrays.splice(minLengthArrIndex, 1);
-    const othArray = arrays;
-    for (let i = 0; i < minLengthArr.length; i++) {
-        const isEventHas = othArray.every(array => array.includes(minLengthArr[i]));
-        if(isEventHas) {
-            result.push(minLengthArr[i]);
+    return minLengthArrIndex;
+}
+
+function splitArrayWithMinIndex<T>(arrays: Array<T>, minIndex): {minLengthArray: T, othArrays: Array<T>} {
+    return {
+        minLengthArray: arrays[minIndex],
+        othArrays: arrays.filter((_, index) => index !== minIndex)
+    }
+}
+
+function intersectionByKey(...arg) {
+    const result = [];
+    const arrays = arg.slice(0, arg.length - 1);
+    if (arrays.length < 2) return result;
+    const key = arg[arg.length - 1];
+    const minLengthArrIndex = getArrayMinLengthIndex(arrays);
+    const { minLengthArray, othArrays } = splitArrayWithMinIndex(arrays, minLengthArrIndex);
+    for(let i = 0; i < minLengthArray.length; i++) {
+        const comparator = (array) => array.find(item => item[key] === minLengthArray[i][key]);
+        console.log(minLengthArray[i][key])
+        const isEqual = othArrays.every(comparator);
+        if (isEqual) {
+            result.push(minLengthArray[i])
         }
     }
     return result;
 }
 
-console.log(intersection([5],[1,2,3,4,5],[3,5]))
+const result = intersectionByKey(
+    [{x: 1, y: 3}, {x: 5, y: 5}],
+    [{x: 1, y: 5}, {x: 3, y: 1}, {x: 5, y: 1}, {x: 3, y: 5}],
+    [{x: 1, y: 7}, {x: 3, y: 3}], 
+    [{x: 3, y: 5}, {x: 5}], 
+    'x');
+console.log(result)
